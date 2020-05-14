740649086c3f3c2a52afd21a1e5a25f495b52e79
Ketan Sahu <ketankumar120@gmail.com>
2020-05-15 02:05:51 +0530

    preprocessing for ml added

--- S&P_list.py
+++ S&P_list.py
@@ -1,12 +1,17 @@
 import bs4 as bs
 import datetime as dt
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 from pandas_datareader import data as pdr
 import pickle
 import requests
 import fix_yahoo_finance as yf
 
 yf.pdr_override
+style.use('ggplot')
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -45,5 +50,82 @@ def get_data_from_yahoo(reload_sp500=False):
             print('Already have {}'.format(ticker))
 
 
-get_data_from_yahoo()
+# get_data_from_yahoo()
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+    main_df = pd.DataFrame()
+    for count, ticker in enumerate(tickers[:25]):
+        df = pd.read_csv(f"stock_dfs/{ticker}.csv")
+        df.set_index('Date', inplace=True)
+        df.rename(columns = {'Adj Close':ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')            
+        
+        if count%10==0:
+            print(count)
+            
+    print(main_df.head(5))
+    main_df.to_csv('sp25_joined_closes.csv')
+    
+    
+# compile_data()
+
+
+def visualize_data():
+    df = pd.read_csv('sp25_joined_closes.csv')
+    df_corr = df.corr()     #find the corelation between the values
+    print(df_corr.head(5))
+    data = df_corr.values
+    fig = plt.figure()      # now we can refer to the graph by fig
+    ax = fig.add_subplot(1,1,1)
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1,1)  # set the color limit within -1 to 1. red is towards -1 and green is toward +1
+    plt.tight_layout()      # to clean the graph
+    
+
+visualize_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
